42b25a4f5efbdf265ec8135faffe186a29768f14
Kaustubh Chaudhuri <kaustubhc2014@gmail.com>
2025-10-04 23:46:51 +0530

    Kmeans - fit/predict function

--- models/kmeans.py
+++ models/kmeans.py
@@ -1,20 +1,56 @@
 """
-
+A basic K-Means Clustering
 """
 
-import random
+import numpy as np
 
-class Centroid:
-    def __init__(self, location):
-        self.location = location
-        self.closest_points = set()
+class KMeansBasic:
+    def __init__(self, n_clusters, max_iter=100, tol=1e-4, random_state=None):
+        self.n_clusters = int(n_clusters)
+        self.max_iter = int(max_iter)
+        self.tol = float(tol)
+        self.random_state = random_state
+        self.cluster_centers_ = None
+        self.labels_ = None
 
-    def get_k_means():
+    def _init_centers(self, X):
+        rng = np.random.default_rng(self.random_state)
+        idx = rng.choice(len(X), size=self.n_clusters, replace=False)
+        return X[idx].copy()
 
-        random.seed(33)
+    def _pairwise_sq_dists(self, A, B):
+        # Matrix of squared euclidean distances
+        return ((A[:, None, :] - B[None, :, :]) ** 2).sum(axis=2)
 
-        initial_centroid_points = random.sample(sorted(list(user_feature_map.keys())), k)
-        centroids = {i:user_feature_map[initial_centroid_users[i]] for i in range(k)}
+    def fit(self, X):
+        X = np.asarray(X, dtype=float)
+        centers = self._init_centers(X)
 
-        for _ in range(10):
+        for _ in range(self.max_iter):
             
+            dist2 = self._pairwise_sq_dists(X, centers)
+            labels = np.argmin(dist2, axis=1)
+
+            
+            new_centers = centers.copy()
+            for j in range(self.n_clusters):
+                mask = labels == j
+                if np.any(mask):
+                    new_centers[j] = X[mask].mean(axis=0)
+                
+
+            
+            shift = np.linalg.norm(new_centers - centers)
+            centers = new_centers
+            if shift < self.tol:
+                break
+
+        self.cluster_centers_ = centers
+        self.labels_ = labels
+        return self
+
+    def predict(self, X):
+        X = np.asarray(X, dtype=float)
+        dist2 = self._pairwise_sq_dists(X, self.cluster_centers_)
+        return np.argmin(dist2, axis=1)
+
