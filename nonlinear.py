483142ba5cbf4636203644398217924f30e0c1a2
Kaustubh Chaudhuri <kaustubhc2014@gmail.com>
2025-10-10 21:27:44 +0530

    Adding non linear functions

---
+++ nonlinear.py
@@ -0,0 +1,28 @@
+"""
+Collection of non linear functions
+"""
+
+import numpy as np
+
+
+class NonLinear():
+
+    def softmax():
+
+    def sigmoid():
+
+    def relu(x):
+
+        """
+        Rectified Linear Unit
+        
+        Args:
+            x (float or np.ndarray): Input value or array.
+
+        Returns:
+            np.ndarray: Output after applying ReLU.
+        """
+        
+        return np.maximum(0, x)
+
+    def leaky_relu():
